c846a8b527e1229c39a5c4c13cdab1519021ccf9
johnclamy <johnclamy@gmail.com>
2026-05-12 12:17:27 +0100

    set the endpoints related to crud operations on explorer data

--- src/web/explorer.py
+++ src/web/explorer.py
@@ -1,9 +1,38 @@
 from fastapi import APIRouter
+from model.explorer import Explorer
+from mockdata import explorer as service
 
 
 router = APIRouter(prefix="/explorer")
 
 
 @router.get('/')
-async def explorer_root():
-    return {"message": "Welcome to the Explorer!"}
+async def get_explorers() -> list[Explorer]:
+    return await service.get_explorers()
+
+
+@router.get('/{name}')
+async def get_explorer(name: str) -> Explorer | None:
+    return await service.get_explorer(name)
+
+
+# all remaining endpoints do nothing yet:
+
+@router.post('/')
+async def add(explorer: Explorer) -> Explorer:
+    return await service.add(explorer)
+
+
+@router.patch('/')
+async def update(name: str, explorer: Explorer) -> Explorer:
+    return await service.update(name, explorer)
+
+
+@router.put('/')
+async def replace(name: str, explorer: Explorer) -> Explorer:
+    return await service.update(name, explorer)
+
+
+@router.delete('/{name}')
+async def delete(name: str) -> bool:
+    return await service.delete(name)
