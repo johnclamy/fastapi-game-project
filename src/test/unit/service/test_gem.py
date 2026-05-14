dd538d78705bf04f92e3f45ac436a6d8627a4ca9
johnclamy <johnclamy@gmail.com>
2026-05-14 13:36:44 +0100

    set unit tests for adding and getting gems

--- src/test/unit/service/test_gem.py
+++ src/test/unit/service/test_gem.py
@@ -10,3 +10,21 @@ sample = Gem(
     value=50000,
     description="A precious gemstone known for its brilliance and hardness."
 )
+
+
+async def test_create():
+    """Test adding a gem."""
+    reslt = await code.add(sample)
+    assert reslt == sample
+
+
+async def test_get_exists():
+    """Test getting an existing gem by name."""
+    reslt = await code.get_gem_by_name(sample.name)
+    assert reslt == sample
+
+
+async def test_get_not_exists():
+    """Test getting a non-existent gem by name."""
+    reslt = await code.get_gem_by_name("Turquoise")
+    assert reslt is None
