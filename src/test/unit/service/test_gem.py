c3b09c5cf2d8008a08a6cf9ef65bda52725fc41a
johnclamy <johnclamy@gmail.com>
2026-05-14 13:12:13 +0100

    setup the test_gem.py for service and instantiate a sample gem

---
+++ src/test/unit/service/test_gem.py
@@ -0,0 +1,12 @@
+from model.gem import Gem
+from service import gem as code
+
+
+sample = Gem(
+    name="Diamond",
+    country_of_origin="South Africa",
+    area_of_discovery="Kimberley",
+    color="Clear",
+    value=50000,
+    description="A precious gemstone known for its brilliance and hardness."
+)
